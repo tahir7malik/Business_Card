5b196d9a1984643b11a5eba4eeecadb636215bf5
tahir7malik <tahir.malik296@gmail.com>
2024-08-15 12:29:49 +0530

    added android logo image and started working on app foundation

--- app/src/main/java/com/example/business_card/MainActivity.kt
+++ app/src/main/java/com/example/business_card/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.business_card.ui.theme.Business_CardTheme
 
@@ -19,29 +25,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Business_CardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Business_Card(firstname: String, middlename: String, lastname: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = firstname,
+            modifier = Modifier.background(Color.Cyan)
+        )
+        Text(
+            text = middlename,
+            modifier = Modifier.background(Color.Yellow)
+        )
+        Text(
+            text = lastname,
+            modifier = Modifier.background(Color.Green)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Business_CardPreview() {
     Business_CardTheme {
-        Greeting("Android")
+        Business_Card(firstname = "Tahir", middlename = "Ahmed", lastname = "Malik")
     }
 }
